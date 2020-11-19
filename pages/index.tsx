1ea9c7606d504bac07eb69079712036f7a64922b
Akash Joshi <jakash1997@gmail.com>
2020-11-19 09:02:59 +0530

    Add ant components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,74 @@
-import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
-import React, { useState } from 'https://esm.sh/react'
-import { Button } from "https://esm.sh/antd";
+import { Import, useDeno } from "https://deno.land/x/aleph/mod.ts";
+import React, { useState } from "https://esm.sh/react";
+import { Breadcrumb, Layout, Menu } from "https://esm.sh/antd";
 
-import Logo from '../components/logo.tsx'
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "https://esm.sh/@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+import "../style/index.less";
 
 export default function Home() {
-    const [count, setCount] = useState(0)
-    const version = useDeno(() => {
-        return Deno.version
-    })
+  const version = useDeno(() => {
+    return Deno.version;
+  });
 
-    return (
-        <div className="page">
-            <Import from="../style/index.less" />
-            <p className="logo"><Logo /></p>
-            <h1>Welcome to use <strong>Aleph.js</strong>!</h1>
-            <p className="links">
-                <a href="https://alephjs.org" target="_blank">Website</a>
-                <span>&middot;</span>
-                <a href="https://alephjs.org/docs/get-started" target="_blank">Get Started</a>
-                <span>&middot;</span>
-                <a href="https://alephjs.org/docs" target="_blank">Docs</a>
-                <span>&middot;</span>
-                <a href="https://github.com/postui/alephjs" target="_blank">GitHub</a>
-            </p>
-            <p className="counter">
-                <span>Counter:</span>
-                <strong>{count}</strong>
-                <button onClick={() => setCount(n => n - 1)}>-</button>
-                <button onClick={() => setCount(n => n + 1)}>+</button>
-            </p>
-            <Button>Hello</Button>
-            <p className="copyinfo">Built by Aleph.js in Deno v{version.deno}</p>
-        </div>
-    )
+//   const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        // collapsed={collapsed}
+        // onCollapse={() => setCollapsed(!collapsed)}
+      >
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 }
